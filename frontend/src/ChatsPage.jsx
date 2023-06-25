739e05de2e287d6ec416714178d9277d8a2ea828
Ernest Opara <ernest.ebuka@gmail.com>
2023-06-25 18:02:29 -0500

    env credentials

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,18 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
-  };
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+        // eslint-disable-next-line react/prop-types
+        username={props.user.username} // adam
+        // eslint-disable-next-line react/prop-types
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
 export default ChatsPage;
